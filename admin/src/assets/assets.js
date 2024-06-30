668cb0ecb336bc1ab08a100b3646f51d71603807
PAiN <91981223+PAAiiN@users.noreply.github.com>
2024-07-01 02:04:17 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 export const currency = "₹"
-export const url = 'http://localhost:4000'
+export const url = 'https://food-site-backend-4zaq.onrender.com'
 
 export const assets ={
     logo,
